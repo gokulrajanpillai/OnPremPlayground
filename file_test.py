0731a5172438eaae9eb576d34bb46e3c7a96fed6
Gokul Rajan <gokulrajanpillai@gmail.com>
2025-11-10 13:23:04 +0000

    Add parameterized tests for avg function

--- file_test.py
+++ file_test.py
@@ -1,6 +1,18 @@
 # Execute by running pytest -q
-
+import pytest
 from file import avg
+
+@pytest.mark.parameterize(
+  "nums, expected",
+  [
+    ([1,2,3], 2.0),
+    ([], None)
+  ],
+  ids=["ints", "empty"]
+)
+
+def test_avg_param(nums, expected):
+  assert avg(nums) == expected
 
 def test_avg():
   assert avg([1,2,3]) == 2.0
